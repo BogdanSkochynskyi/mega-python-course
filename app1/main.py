fb36102d980e014489dd8b79644e3ec04cc0f05f
bogdanskochynskyi <bogdanskochynskyi@gmail.com>
2026-03-21 11:49:38 +0200

    Day 2-3 finished.

--- app1/main.py
+++ app1/main.py
@@ -1,6 +1,19 @@
-user_prompt = "Enter a todo:"
-todo1 = input(user_prompt)
-todo2 = input(user_prompt)
-todo3 = input(user_prompt)
-todos = [todo1, todo2, todo3]
-print(todos)
+todos = []
+
+while True:
+    user_action = input("Type add, show or exit: ")
+    user_action = user_action.strip().lower()
+
+    match user_action:
+        case 'add':
+            todo = input("Enter a todo: ")
+            todos.append(todo)
+        case 'show' | 'display':
+            for list_item in todos:
+                    print(list_item)
+        case 'exit':
+            break
+        case _:
+            print("Invalid input")
+
+print("Goodbye!")
