1e43269af499acab4d5a78138e6739296e048003
bogdanskochynskyi <bogdanskochynskyi@gmail.com>
2026-04-02 13:04:50 +0300

    Section 6 finished. Read and save data in file

--- app1/main.py
+++ app1/main.py
@@ -1,16 +1,20 @@
-todos = []
-
 while True:
     user_action = input("Type add, show, edit, complete or exit: ")
     user_action = user_action.strip().lower()
+    file = open("todos.txt", "r")
+    todos = file.readlines()
+    file.close()
 
     match user_action:
         case 'add':
-            todo = input("Enter a todo: ")
+            todo = input("Enter a todo: ") + "\n"
             todos.append(todo)
+            file = open("todos.txt", "w")
+            file.writelines(todos)
+            file.close()
         case 'show' | 'display':
             for index, list_item in enumerate(todos):
-                print(f"{index + 1}.{list_item}")
+                print(f"{index + 1}.{list_item.strip()}")
         case 'edit':
             number = int(input("Write a number of todo item to edit "))
             new_text = input(f"You want to edit {todos[int(number) - 1]}, write new todo text ")
