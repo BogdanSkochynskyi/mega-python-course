50fbb67ab38b7ceb9549845186aa2a51888d7362
bogdanskochynskyi <bogdanskochynskyi@gmail.com>
2026-04-07 10:15:08 +0300

    Section 8 and 9 finished. Modifying code to reduce inputs

--- app1/main.py
+++ app1/main.py
@@ -1,61 +1,60 @@
 while True:
-    user_action = input("Type add, show, edit, complete or exit: ")
+    user_action = input("Type add [Task], show, edit [number], complete [number] or exit: ")
     user_action = user_action.strip().lower()
     todos = []
 
-    match user_action:
-        case 'add':
-            todo = input("Enter a todo: ") + "\n"
+    if 'add' in user_action:
+        todo = user_action[4:] + '\n'
 
-            with open("todos.txt", "r") as file:
-                todos = file.readlines()
+        with open("todos.txt", "r") as file:
+            todos = file.readlines()
 
-            todos.append(todo)
+        todos.append(todo)
 
-            with open("todos.txt", "w") as file:
-                file.writelines(todos)
+        with open("todos.txt", "w") as file:
+            file.writelines(todos)
 
-        case 'show' | 'display':
+    elif 'show' in user_action:
 
-            # new_todos = []
-            # for item in todos:
-            #     new_todos.append(item.strip('\n'))
-            # Same:
-            # new_todos = [item.strip('\n') for item in todos]
-            with open("todos.txt", "r") as file:
-                todos = file.readlines()
+        # new_todos = []
+        # for item in todos:
+        #     new_todos.append(item.strip('\n'))
+        # Same:
+        # new_todos = [item.strip('\n') for item in todos]
+        with open("todos.txt", "r") as file:
+            todos = file.readlines()
 
-            for index, item in enumerate(todos):
-                item = item.strip('\n')
-                row = f"{index + 1}.{item}"
-                print(row)
-        case 'edit':
-            number = int(input("Write a number of todo item to edit "))
+        for index, item in enumerate(todos):
+            item = item.strip('\n')
+            row = f"{index + 1}.{item}"
+            print(row)
+    elif 'edit' in user_action:
+        number = int(user_action[5:])
 
-            with open("todos.txt", "r") as file:
-                todos = file.readlines()
+        with open("todos.txt", "r") as file:
+            todos = file.readlines()
 
-            new_text = input(f"You want to edit {todos[int(number) - 1].strip('\n')} write new todo text ") + '\n'
-            todos[number - 1] = new_text
+        new_text = input(f"You want to edit {todos[int(number) - 1].strip('\n')} write new todo text ") + '\n'
+        todos[number - 1] = new_text
 
-            with open("todos.txt", "w") as file:
-                file.writelines(todos)
+        with open("todos.txt", "w") as file:
+            file.writelines(todos)
 
-        case 'complete':
-            number = int(input("Write a number of todo item to complete "))
+    elif 'complete' in user_action:
+        number = int(user_action[9:])
 
-            with open("todos.txt", "r") as file:
-                todos = file.readlines()
+        with open("todos.txt", "r") as file:
+            todos = file.readlines()
 
-            completed_item = todos.pop(number - 1)
+        completed_item = todos.pop(number - 1)
 
-            with open("todos.txt", "w") as file:
-                file.writelines(todos)
+        with open("todos.txt", "w") as file:
+            file.writelines(todos)
 
-            print(f"You completed {completed_item}")
-        case 'exit':
-            break
-        case _:
-            print("Invalid input")
+        print(f"You completed {completed_item}")
+    elif 'exit' in user_action:
+        break
+    else:
+        print("Invalid input")
 
 print("Goodbye!")
