8210f3a6783e49845ed442839467654ec67f7f33
bogdanskochynskyi <bogdanskochynskyi@gmail.com>
2026-04-08 19:35:57 +0300

    Section 10 finished. Add catching exceptions and fix possible bugs in commands

--- app1/main.py
+++ app1/main.py
@@ -3,7 +3,7 @@ while True:
     user_action = user_action.strip().lower()
     todos = []
 
-    if 'add' in user_action:
+    if  user_action.startswith('add'):
         todo = user_action[4:] + '\n'
 
         with open("todos.txt", "r") as file:
@@ -14,7 +14,7 @@ while True:
         with open("todos.txt", "w") as file:
             file.writelines(todos)
 
-    elif 'show' in user_action:
+    elif user_action.startswith('show') :
 
         # new_todos = []
         # for item in todos:
@@ -28,31 +28,45 @@ while True:
             item = item.strip('\n')
             row = f"{index + 1}.{item}"
             print(row)
-    elif 'edit' in user_action:
-        number = int(user_action[5:])
+    elif user_action.startswith('edit') :
+        try:
+            number = int(user_action[5:])
 
-        with open("todos.txt", "r") as file:
-            todos = file.readlines()
+            with open("todos.txt", "r") as file:
+                todos = file.readlines()
 
-        new_text = input(f"You want to edit {todos[int(number) - 1].strip('\n')} write new todo text ") + '\n'
-        todos[number - 1] = new_text
+            new_text = input(f"You want to edit {todos[int(number) - 1].strip('\n')} write new todo text ") + '\n'
+            todos[number - 1] = new_text
 
-        with open("todos.txt", "w") as file:
-            file.writelines(todos)
+            with open("todos.txt", "w") as file:
+                file.writelines(todos)
+        except ValueError:
+            print("Invalid input. Use number of task to edit.")
+            continue
+        except IndexError:
+            print("Invalid index.")
+            continue
 
-    elif 'complete' in user_action:
-        number = int(user_action[9:])
+    elif user_action.startswith('complete') :
+        try:
+            number = int(user_action[9:])
 
-        with open("todos.txt", "r") as file:
-            todos = file.readlines()
+            with open("todos.txt", "r") as file:
+                todos = file.readlines()
 
-        completed_item = todos.pop(number - 1)
+            completed_item = todos.pop(number - 1)
 
-        with open("todos.txt", "w") as file:
-            file.writelines(todos)
+            with open("todos.txt", "w") as file:
+                file.writelines(todos)
 
-        print(f"You completed {completed_item}")
-    elif 'exit' in user_action:
+            print(f"You completed {completed_item}")
+        except ValueError:
+            print("Invalid input. Use number of task to complete.")
+            continue
+        except IndexError:
+            print("Invalid index.")
+            continue
+    elif user_action.startswith('exit') :
         break
     else:
         print("Invalid input")
