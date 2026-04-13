93b80e6f7cf00f036325349d5f30331293c31b4d
bogdanskochynskyi <bogdanskochynskyi@gmail.com>
2026-04-13 10:00:39 +0300

    Section 14 finished. Add module for functions and move functions to it

--- app1/main.py
+++ app1/main.py
@@ -1,19 +1,6 @@
-TODOS_TXT = "todos.txt"
+# from functions import get_todos, write_todos
 
-
-def get_todos(filepath=TODOS_TXT):
-    """ Return a list of todos from a file. """
-    with open(filepath, "r") as file_local:
-        todos_local = file_local.readlines()
-    return todos_local
-
-
-def write_todos(local_todos, filepath=TODOS_TXT):
-    """ Write a list of todos to a file. """
-    with open(filepath, "w") as local_file:
-        local_file.writelines(local_todos)
-
-# print(help(get_todos))
+from modules import functions
 
 while True:
     user_action = input("Type add [Task], show, edit [number], complete [number] or exit: ")
@@ -25,13 +12,13 @@ while True:
 
         # todos = get_todos(filepath=TODOS_TXT)
         # todos = get_todos(new_todos.txt)
-        todos = get_todos()
+        todos = functions.get_todos()
 
         todos.append(todo)
 
         # write_todos(filepath=TODOS_TXT, local_todos=todos)
         # write_todos(todos, "new_todos.txt")
-        write_todos(todos)
+        functions.write_todos(todos)
 
     elif user_action.startswith('show'):
 
@@ -40,7 +27,7 @@ while True:
         #     new_todos.append(item.strip('\n'))
         # Same:
         # new_todos = [item.strip('\n') for item in todos]
-        todos = get_todos()
+        todos = functions.get_todos()
 
         for index, item in enumerate(todos):
             item = item.strip('\n')
@@ -50,12 +37,12 @@ while True:
         try:
             number = int(user_action[5:])
 
-            todos = get_todos()
+            todos = functions.get_todos()
 
             new_text = input(f"You want to edit {todos[int(number) - 1].strip('\n')} write new todo text ") + '\n'
             todos[number - 1] = new_text
 
-            write_todos(todos)
+            functions.write_todos(todos)
         except ValueError:
             print("Invalid input. Use number of task to edit.")
             continue
@@ -67,11 +54,11 @@ while True:
         try:
             number = int(user_action[9:])
 
-            todos = get_todos()
+            todos = functions.get_todos()
 
             completed_item = todos.pop(number - 1)
 
-            write_todos(todos)
+            functions.write_todos(todos)
 
             print(f"You completed {completed_item}")
         except ValueError:
---
+++ app1/modules/functions.py
@@ -0,0 +1,15 @@
+TODOS_TXT = "todos.txt"
+
+def get_todos(filepath=TODOS_TXT):
+    """ Return a list of todos from a file. """
+    with open(filepath, "r") as file_local:
+        todos_local = file_local.readlines()
+    return todos_local
+
+
+def write_todos(local_todos, filepath=TODOS_TXT):
+    """ Write a list of todos to a file. """
+    with open(filepath, "w") as local_file:
+        local_file.writelines(local_todos)
+
+# print(help(get_todos))
