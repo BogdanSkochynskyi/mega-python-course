f7d827de6857ab1aff8c0d9978c31ec1c66c433b
bogdanskochynskyi <bogdanskochynskyi@gmail.com>
2026-04-20 20:50:14 +0300

    Section 18. Add exit button and black theme for coding exercise

--- coding_exercises/day17_1.py
+++ coding_exercises/day17_1.py
@@ -2,19 +2,22 @@ import FreeSimpleGUI as sg
 
 from modules import functions
 
+sg.theme('DarkBlue')
+
 label1 = sg.Text("Enter feet:")
 input_box1 = sg.InputText(key="feets")
 label2 = sg.Text("Enter inches:")
 input_box2 = sg.InputText(key="inches")
 convert_button = sg.Button("Convert")
 label3 = sg.Text(key='result')
+exit_button = sg.Button("Exit")
 
 def convert(feet, inches):
     meters = feet * 0.3048 + inches * 0.0254
     return meters
 
 window = sg.Window('Convertor',
-                   layout=[[label1, input_box1], [label2, input_box2], [convert_button, label3]],
+                   layout=[[label1, input_box1], [label2, input_box2], [convert_button, exit_button, label3]],
                    font=('Helvetica', 20))
 
 
@@ -24,6 +27,8 @@ while True:
         case "Convert":
             met = convert(float(values['feets']), float(values['inches']))
             window['result'].update(value=met)
+        case "Exit":
+            break
         case sg.WIN_CLOSED:
             break
 
